54861115dbd1f9c5401b828b703a37db54364f1e
Oguama <oguamaprosperity@gmail.com>
2025-02-13 02:02:37 +0200

    add docstrings to augmentations.py

--- src/utils/augmentations.py
+++ src/utils/augmentations.py
@@ -1,13 +1,51 @@
 import numpy as np
 from scipy.ndimage import rotate, shift
 
-def apply_translation(image, translation):
-    return shift(image, shift=(translation[0], translation[1], 0), mode='nearest')
 
-def apply_rotation(image, angle):
+def apply_translation(image: np.ndarray,
+                      translation: tuple[float, float]) -> np.ndarray:
+    """
+    Applies translation to an MRI image.
+
+    Parameters:
+    image (np.ndarray): The MRI image in array format.
+    translation (tuple[float, float]): The translation offsets in the x and y directions.
+
+    Returns:
+    np.ndarray: The translated MRI image.
+    """
+    return shift(image,
+                 shift=(translation[0], translation[1], 0),
+                 mode='nearest')
+
+
+def apply_rotation(image: np.ndarray, angle: float) -> np.ndarray:
+    """
+    Rotates an MRI image by a given angle.
+
+    Parameters:
+    image (np.ndarray): The MRI image in array format.
+    angle (float): The angle of rotation in degrees.
+
+    Returns:
+    np.ndarray: The rotated MRI image.
+    """
     return rotate(image, angle, axes=(0, 1), reshape=False, mode='nearest')
 
-def apply_gaussian_noise(image, mean, std):
+
+def apply_gaussian_noise(image: np.ndarray, mean: float,
+                         std: float) -> np.ndarray:
+    """
+    Adds Gaussian noise to an MRI image.
+
+    Parameters:
+    image (np.ndarray): The MRI image in array format.
+    mean (float): The mean of the Gaussian noise.
+    std (float): The standard deviation factor for the Gaussian noise.
+
+    Returns:
+    np.ndarray: The MRI image with added Gaussian noise.
+    """
     std = image.std() * std
     noise = np.random.normal(mean, std, image.shape).astype(image.dtype)
     return image + noise
